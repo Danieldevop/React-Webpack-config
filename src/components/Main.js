9d5461ab1418c4a7f0aa61268422bff3417ff2cb
Daniel <daniel.sendmemail@gmail.com>
2018-09-25 17:01:14 -0500

    Version 1 - webpack4

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = (props) => {
+	return(
+		<div>
+			<h1>Hello React!</h1>
+			<p>Remove this component and create a forder in your ./src directory call components</p>
+		</div>
+	)
+}
+
+export default Main
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from  'react-dom'
+import Main from './components/Main'
+import style from './assets/main.css'
+
+ReactDOM.render(<Main />, document.getElementById('app'))
